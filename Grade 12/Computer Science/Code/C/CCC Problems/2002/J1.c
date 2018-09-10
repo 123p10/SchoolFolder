633c72955f76baf2ae3502e49399abcc2deed0a8
123p10 <obrake3@gmail.com>
2018-09-09 20:08:42 -0400

    Computer Science Word Doc

---
+++ Grade 12/Computer Science/Code/C/CCC Problems/2002/J1.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+
+int main(void){
+  int input = 0;
+  printf("Enter a digit between 0 and 9 ");
+  scanf("%d",&input);
+  int numbers[10][8] = {
+    {1,1,0,1,1,1,1},
+    {0,1,0,1,0,0,0},
+    {1,1,1,0,1,1,0},
+    {1,1,1,1,1,0,0},
+    {0,1,1,1,0,0,1},
+    {1,0,1,1,1,0,1},
+    {1,0,1,1,1,1,1},
+    {1,1,0,1,0,0,0},
+    {1,1,1,1,1,1,1},
+    {1,1,1,1,1,0,1}
+  };
+  if(numbers[input][0] == 1){
+    printf(" *** ");
+  }
+  printf("\n");
+
+  if(numbers[input][1] == 1 && numbers[input][6]){
+    printf("*   *\n");
+    printf("*   *\n");
+    printf("*   *\n");
+  }
+  else if(numbers[input][1] == 1){
+    printf("    *\n");
+    printf("    *\n");
+    printf("    *\n");
+  }
+  else if(numbers[input][6]){
+    printf("*\n");
+    printf("*\n");
+    printf("*\n");
+  }
+  if(numbers[input][2]){
+    printf(" *** \n");
+  }
+  else{
+    printf("\n");
+  }
+  if(numbers[input][3] == 1 && numbers[input][5]){
+    printf("*   *\n");
+    printf("*   *\n");
+    printf("*   *\n");
+  }
+  else if(numbers[input][3] == 1){
+    printf("    *\n");
+    printf("    *\n");
+    printf("    *\n");
+  }
+  else if(numbers[input][5]){
+    printf("*\n");
+    printf("*\n");
+    printf("*\n");
+  }
+  if(numbers[input][4]){
+    printf(" *** \n");
+  }
+  return 0;
+
+}
---
+++ Grade 12/Computer Science/Code/C/Euclid's Algorithim/Euclid.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+
+/*
+This function finds the greatest number that when multiplied to the numerator will result in an integer that is less then the denominator
+*/
+int multiple(int num, int den);
+int gcd(int numerator,int denominator);
+
+
+
+int main(void) {
+
+  //  I/O
+  int numerator, denominator;
+  printf("Enter a positive integer for the numerator ");
+   scanf("%d", &numerator);
+  printf("Enter a positive integer for the denominator ");
+  scanf("%d", &denominator);
+  printf("\n");
+  //Calculate the first multiple
+  int gcf = gcd(numerator,denominator);
+  //Output
+  printf("GCF: %d \n\n",gcf);
+  printf("%d / %d",numerator/gcf,denominator/gcf);
+  //End Program
+  return 0;
+}
+int multiple(int num, int den)
+{
+ int a = 1;
+ while( a*num <= den ) a++;
+ return a-1;
+}
+int gcd(int numerator,int denominator){
+  int remainder;
+  int a = multiple(numerator, denominator);
+
+  //Euclids algorithim breaks out when it reachs 0 so the while loop breaks when this happens
+  while ((denominator - a*numerator) != 0)
+  {
+    //This is Euclids method
+    //denominator = numerator * a + remainder
+    //thus
+    //remainder = denominator - numerator*a
+    remainder = denominator - a*numerator;
+
+    //We then reset the values to call multiple again and loop
+    denominator = numerator;
+    numerator = remainder;
+    //Calculate the next multiple
+    a = multiple(numerator, denominator);
+  }
+  return numerator;
+}
