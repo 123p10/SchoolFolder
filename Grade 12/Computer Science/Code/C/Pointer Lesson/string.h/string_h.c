7a76474859fc50e4b1e6f30e6e8c71652ba3635c
123p10 <obrake3@gmail.com>
2018-12-18 20:39:29 -0500

    more comp sci

--- Grade 12/Computer Science/Code/C/Pointer Lesson/string.h/string_h.c
+++ Grade 12/Computer Science/Code/C/Pointer Lesson/string.h/string_h.c
@@ -5,6 +5,25 @@
 //Make sure to add comments
 
 int main(void) {
-  mainInput();
+  char* arr = "bonjour";
+  promptForBinaryString(arr);
+  printf("%s",arr);
+  char *inputString = "110101010111110101111110";
+  char letters[20] = {'C','B','A','F'};
+  char *values[20] = {"0","111","11010","10"};
+  char *curr= (char*)calloc(250, sizeof(char));
+  struct node* node = constructBinaryTree(letters,values);
+  int count = 0;
+  char value;
+  for(int i = 0;i < str_len(inputString);i++){
+    curr[count] = inputString[i];
+    value = getValueAtCode(node,curr);
+    if(value != '\0'){
+      printf("%c",value);
+      curr = (char*)calloc(250, sizeof(char));
+      count = 0;
+    }
+    else{count++;}
+  }
   return 0;
 }
